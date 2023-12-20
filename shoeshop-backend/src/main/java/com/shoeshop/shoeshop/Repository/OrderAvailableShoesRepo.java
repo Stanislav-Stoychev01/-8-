8db324ac165fa2fec07f01687948be4cb0d86232
SSTOYCH <stanislav.stoychev@p3-group.com>
2023-12-20 15:57:24 +0200

    Fix BE

--- shoeshop-backend/src/main/java/com/shoeshop/shoeshop/Repository/OrderAvailableShoesRepo.java
+++
@@ -1,8 +0,0 @@
-package com.shoeshop.shoeshop.Repository;
-
-import com.shoeshop.shoeshop.Entity.Order_AvailableShoes;
-import com.shoeshop.shoeshop.Keys.OrderAvailableShoesCompositeKey;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderAvailableShoesRepo extends JpaRepository<Order_AvailableShoes, OrderAvailableShoesCompositeKey> {
-}
